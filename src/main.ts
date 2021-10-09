d0a096f53bf37d49940328fc435cec3cf8bd4013
Garfield550 <openpgp@550.moe>
2021-10-09 12:20:13 +0000

    Revert "chore: try log env"
    This reverts commit cfc1cdef7f2d62ee381f7457df45c6f3c1f4f1a4.

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,6 @@ import { startTask } from './task';
 async function bootstrap(): Promise<void> {
   const http = getBackendService();
   logger.info('App started');
-  console.log(process.env);
-  logger.info(JSON.stringify(process.env));
   await http.reportWorkerTaskStartedToBackend();
 
   timer
