45482e2c0c6da9f1f7835cec9bfef3d2a8e2baaa
Garfield550 <openpgp@550.moe>
2021-10-13 09:13:18 +0000

    pref: better task workflow

--- src/task/index.ts
+++ src/task/index.ts
@@ -1,4 +1,4 @@
-import { BackendTaskService, checkAllowedTasks } from '@metaio/worker-common';
+import { checkAllowedTasks } from '@metaio/worker-common';
 import { MetaWorker } from '@metaio/worker-model';
 
 import { getBackendService } from '../api';
@@ -18,11 +18,6 @@ export const startTask = async (): Promise<void> => {
   const taskConf = await http.getWorkerTaskFromBackend<MixedTaskConfig>();
   if (!taskConf) throw Error('Can not get task config from backend or gateway');
 
-  const commonDoing = async (http: BackendTaskService): Promise<void> => {
-    await http.reportWorkerTaskFinishedToBackend();
-    loggerService.final('Task finished');
-  };
-
   const { taskId, taskMethod } = taskConf?.task;
   logger.info(`Task id ${taskId} start, method ${taskMethod}`);
 
@@ -33,41 +28,28 @@ export const startTask = async (): Promise<void> => {
 
   if (taskMethod === MetaWorker.Enums.TaskMethod.HEXO_UPDATE_CONFIG) {
     logger.info(`Starting task updateHexoConfigFiles`);
-
     await hexoService.updateHexoConfigFiles();
-
     logger.info(`Task updateHexoConfigFiles finished`);
-
-    await commonDoing(http);
   }
 
   if (taskMethod === MetaWorker.Enums.TaskMethod.HEXO_GENERATE_DEPLOY) {
     logger.info(`Starting task generateHexoStaticFiles`);
-
     await hexoService.generateHexoStaticFiles();
-
     logger.info(`Task generateHexoStaticFiles finished`);
-
-    await commonDoing(http);
   }
 
   if (taskMethod === MetaWorker.Enums.TaskMethod.HEXO_CREATE_POST) {
     logger.info(`Starting task createHexoPostFile`);
-
     await hexoService.createHexoPostFiles();
-
     logger.info(`Task createHexoPostFile finished`);
-
-    await commonDoing(http);
   }
 
   if (taskMethod === MetaWorker.Enums.TaskMethod.HEXO_UPDATE_POST) {
     logger.info(`Starting task createHexoPostFile, replase true`);
-
     await hexoService.createHexoPostFiles(true);
-
     logger.info(`Task createHexoPostFile finished`);
-
-    await commonDoing(http);
   }
+
+  await http.reportWorkerTaskFinishedToBackend();
+  loggerService.final('Task finished');
 };
